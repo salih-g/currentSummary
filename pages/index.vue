7c608f4ed1eccdd49be4cde4bd08fb072db81f30
Salih Gumus <sudanmerinosu@gmail.com>
2022-01-23 18:05:00 +0300

    card design

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,72 @@
 <template>
   <div class="container">
     <h1 class="title">Current Summary</h1>
-    <form class="form">
-      <div class="inputs">
-        <input type="text" id="title" placeholder="Title" class="input" />
-        <textarea
-          type="text"
-          id="question"
-          rows="5"
-          class="input-textarea"
-          placeholder="Your question"
-        />
-      </div>
-      <input type="submit" value="Submit" class="buttton" />
-    </form>
+    <h3 v-if="postCreated" class="postCreated">Post Created</h3>
+    <div class="inputs">
+      <input
+        type="text"
+        id="title"
+        placeholder="Title"
+        class="input"
+        v-model="postData.title"
+      />
+      <textarea
+        type="text"
+        id="question"
+        rows="5"
+        class="input-textarea"
+        placeholder="What is your question?"
+        v-model="postData.question"
+      />
+    </div>
+    <input type="submit" value="Submit" class="buttton" @click="handleCreate" />
+
+    <div class="card" v-for="(question, key) in questions" :key="key">
+      <div class="ovelay"></div>
+      <header class="user">
+        <div class="user-info">
+          <h2 class="user-info-name">{{ question.title }}</h2>
+          <p class="user-info-time">4 min ago</p>
+        </div>
+      </header>
+      <main>
+        <p>{{ question.question }}</p>
+      </main>
+      <section>
+        <p class="comment">10 comment</p>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      questions: [],
+      postData: {
+        title: "",
+        question: "",
+      },
+      postCreated: false,
+    };
+  },
+  async created() {
+    await this.$axios.$get("").then((data) => {
+      this.questions = data;
+    });
+  },
+  methods: {
+    async handleCreate() {
+      this.$axios.$post("/", this.postData).then(() => {
+        this.postData.title = "";
+        this.postData.question = "";
+        this.postCreated = true;
+        setTimeout(() => (this.postCreated = false), 2000);
+      });
+    },
+  },
 };
 </script>
 
@@ -29,11 +76,16 @@ body {
   padding: 0;
   margin: 0;
   font-family: "Tomorrow", sans-serif;
+  overflow-x: hidden;
+  background-color: #1c1e2c;
 }
-
 * {
   box-sizing: border-box;
 }
+ul {
+  list-style-type: none;
+}
+
 .container {
   width: 90%;
   margin: 0 auto;
@@ -41,6 +93,11 @@ body {
 }
 .title {
   font-size: 20px;
+  text-transform: capitalize;
+  color: #c6e1ed;
+}
+.postCreated {
+  color: green;
 }
 .inputs {
   width: 50%;
@@ -65,46 +122,58 @@ body {
 
 .buttton {
   padding: 10px 30px;
+  margin-bottom: 10px;
 
-  background-color: #4198d3;
-  color: white;
+  background-color: #c6e1ed;
+  color: black;
 
   border: none;
   border-radius: 5px;
 
   cursor: pointer;
 }
-.title {
-  text-transform: capitalize;
-}
 
-.list {
-  background-color: #eeeeee;
-  border-radius: 10px;
-
-  width: 80%;
-  max-width: 600px;
-
-  padding: 10px;
-  margin: 30px auto;
-}
-
-.linkPreview {
-  max-width: 400px;
-  margin: 0 auto;
+.card {
+  border-radius: 20px;
+  background: #191a1d;
   overflow: hidden;
+  padding: 14px;
+
+  margin: 10px auto;
+  width: 80%;
+}
+.card header.user {
+  display: flex;
+  gap: 12px;
+}
+.card header .user-info-name {
+  letter-spacing: 0.4px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #fff;
+}
+.card header .user-info-time {
+  font-size: 12px;
+  color: rgba(255, 255, 255, 0.588);
+}
+.card main p {
+  font-size: 14px;
+  color: #fff;
+  margin-top: 16px;
+  line-height: 1.7;
+}
+.card section {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
 }
 
-.deleteButton {
-  margin-top: 15px;
-  padding: 5px 80px;
-
-  color: white;
-  font-size: 24px;
-  background-color: rgb(236, 67, 146);
-
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
+.card section p {
+  color: rgba(255, 255, 255, 0.508);
+  font-size: 12px;
+  margin-left: 10px;
+}
+.card section p.comment {
+  margin-left: auto;
 }
 </style>
