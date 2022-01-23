ba91b9c1ac1005732594212b285ccf7b8ef185df
Salih Gumus <sudanmerinosu@gmail.com>
2022-01-23 20:32:20 +0300

    add getById

--- server/routes/questions/index.js
+++ server/routes/questions/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const Question = require("../../models/Question");
 
+//Create
 router.post("/", async (req, res) => {
   const newQuestion = new Question(req.body);
 
@@ -14,11 +15,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Get All
 router.get("/", async (req, res) => {
   try {
     const questions = await Question.find();
 
     res.status(200).json(questions);
+  } catch (err) {
+    res.status(500).json({ error: err.message || err });
+  }
+});
+
+//Get by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const question = await Question.findById(id);
+    res.status(200).json(question);
   } catch (err) {
     res.status(500).json({ error: err.message || err });
   }
